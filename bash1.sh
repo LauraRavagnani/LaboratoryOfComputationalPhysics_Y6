02993e9bae9008995f37f71ad9c9e915dda260da
LauraRavagnani <laura.ravagnani.00@gmail.com>
2023-11-15 16:18:35 +0100

    ex3

--- bash1.sh
+++ bash1.sh
@@ -27,6 +27,7 @@ grep "PoD" LCP_22-23_students.csv > pod.csv
 grep "Physics" LCP_22-23_students.csv > physics.csv
 
 # counts how many students' surname begins with each letter of the alphabet
+# find which letter has more counts
 n_max=0
 letter_max=0
 
@@ -34,18 +35,29 @@ for letter in {A..Z}; do
     n=$(grep -c "^$letter" LCP_22-23_students.csv)
     echo "number of students whose surname start with letter $letter is: $n"
 
-    if [[ $n>$n_max ]]; then
+    if [ $n -gt $n_max ]; then
     	n_max=$n
     	letter_max=$letter
     fi
 done
 echo "the letter that has more counts is $letter_max"
 
-# find which letter has more counts
+# group students "modulo 18", i.e. 1,19,37,.. 2,20,38,.. etc. and put each group in a separate file 
+#n_students=$(wc -l < LCP_22-23_students.csv)    # number of students
 
+#awk 'NR > 1 && (NR-1) % 18 == 1 {print $0 > }' LCP_22-23_students.csv
 
+# Create a directory to store the output files
+output_dir="students"
+mkdir -p "$output_dir"
 
-
-
-
+# Use awk to group rows modulo 18, skipping the header and redirecting to files
+awk -v output_dir="$output_dir" '
+{
+    if ((NR-1) % 18 == 0) {
+        close(output_file)
+        output_file = output_dir "/Group" ++i ".csv"
+    }
+    print $0 > output_file
+}' LCP_22-23_students.csv
 
